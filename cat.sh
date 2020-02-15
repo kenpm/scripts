d4a541136c6f58b848542684c97c19b05fff21ac
Ken McNamee <kenm@live.com>
2020-02-15 13:20:47 -0500

    iTerm2

--- cat.sh
+++ cat.sh
@@ -3,7 +3,10 @@
 # DEV ENVIRONMENT SETUP SCRIPT (Catalina)      #
 #                                              #
 ################################################
+# http://osxdaily.com/2019/01/04/25-of-the-best-terminal-tips-from-2018/
 
+wget https://iterm2.com/downloads/stable/iTerm2-3_3_9.zip
+# need to figure out how to unzip and install iterm2
 
 # ******************************************
 # Install Command Line Developer Tools
@@ -19,3 +22,10 @@ xcode-select --install
 # http://osxdaily.com/2018/03/07/how-install-homebrew-mac-os/
 # ******************************************
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)" to uninstall
+brew analytics off
+brew update
+
+brew install wget
+
+brew install node
