a8143ab1a9928abca8aeacb90154e374dae6e7e2
Ken McNamee <kenm@live.com>
2020-02-27 14:30:32 +0000

    Added chromium-browser and plasma-nm

--- kdemin.sh
+++ kdemin.sh
@@ -2,4 +2,4 @@ sudo apt update
 sudo apt install software-properties-common -yy
 sudo add-apt-repository ppa:kubuntu-ppa/backports -y
 sudo apt update
-sudo apt install open-vm-tools-desktop htop apt-transport-https curl net-tools git kde-plasma-desktop network-manager-openconnect network-manager-vpnc -yy
+sudo apt install open-vm-tools-desktop htop apt-transport-https curl net-tools git chromium-browser kde-plasma-desktop plasma-nm network-manager-openconnect network-manager-vpnc -yy
