7e6f783953694e127d9cf316b6ff528fe343346f
Ken McNamee <kenm@live.com>
2020-02-27 13:45:55 +0000

    First

---
+++ kdemin.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:kubuntu-ppa/backports
+sudo apt update
+sudo apt install open-vm-tools-desktop htop apt-transport-https curl net-tools git kde-plasma-desktop network-manager-openconnect network-manager-vpnc -yy
