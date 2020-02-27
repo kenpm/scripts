9ab7bc512ee23a15f0f374bc608d9f96aadcbad6
Ken McNamee <kenm@live.com>
2020-02-27 13:51:53 +0000

    Added software-properties-common

--- kdemin.sh
+++ kdemin.sh
@@ -1,3 +1,5 @@
+sudo apt update
+sudo apt install software-properties-common
 sudo add-apt-repository ppa:kubuntu-ppa/backports
 sudo apt update
 sudo apt install open-vm-tools-desktop htop apt-transport-https curl net-tools git kde-plasma-desktop network-manager-openconnect network-manager-vpnc -yy
