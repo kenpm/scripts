b0af3f42f5964bece0cba5e26155afcfad810e9a
Ken McNamee <kenm@live.com>
2020-02-28 18:49:12 +0000

    .

--- kdemin.sh
+++ kdemin.sh
@@ -2,4 +2,4 @@ sudo apt update
 sudo apt install software-properties-common -yy
 sudo add-apt-repository ppa:kubuntu-ppa/backports -y
 sudo apt update
-sudo apt install htop apt-transport-https curl net-tools git network-manager-openconnect kde-plasma-desktop plasma-nm muon open-vm-tools-desktop gwenview deepin-screenshot polkit-kde-1 chromium-browser -yy
+sudo apt install htop apt-transport-https curl net-tools git network-manager-openconnect-gnome network-manager-vpnc-gnome kde-plasma-desktop plasma-nm muon open-vm-tools-desktop gwenview deepin-screenshot polkit-kde-1 chromium-browser -yy
