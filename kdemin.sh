e83b802b30c8fb00f79e7f9b60629bd2adaca982
Ken McNamee <kenm@live.com>
2020-02-28 00:20:30 +0000

    gwenview deepin-screenshot polkit-kde-1

--- kdemin.sh
+++ kdemin.sh
@@ -2,4 +2,4 @@ sudo apt update
 sudo apt install software-properties-common -yy
 sudo add-apt-repository ppa:kubuntu-ppa/backports -y
 sudo apt update
-sudo apt install htop apt-transport-https curl net-tools git kde-plasma-desktop plasma-nm network-manager-openconnect network-manager-vpnc open-vm-tools-desktop chromium-browser -yy
+sudo apt install htop apt-transport-https curl net-tools git network-manager-openconnect kde-plasma-desktop plasma-nm muon open-vm-tools-desktop gwenview deepin-screenshot polkit-kde-1 chromium-browser -yy
